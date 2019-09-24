1a13382019aaa1a4a3b2307995b454f60436897c
Matthew Maugeri <matthew.maugeri@students.mq.edu.au>
2019-09-24 16:15:33 +1000

    Fully functional program with use of example codes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ public class Main {
     public static void main (String[] args) {
         WeatherData weatherData = new WeatherData();
 
+        /**
+         * Create the 3 displays (Observers) and pass them the weatherData object 
+         */
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
     }
 }
